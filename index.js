7a9ecfb2349432db0e587d37e4c93033095397ee
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-23 22:32:39 +0600

    uri and async function added from the mongo db database connection

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rbrmt.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected')
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Hello from cpu tech!')
