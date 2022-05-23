11bb1a573deb318d83aa943553f52a86cfe9ffe5
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-23 18:45:41 +0600

    middleware added

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
-
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello from cpu tech!')
