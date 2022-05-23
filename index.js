d78b125f5d3fceac624c03a751c05783e6f7baee
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-23 18:43:38 +0600

    express js added

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('Hello from cpu tech!')
