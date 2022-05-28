af8e0a5882a55fe127bef561e3509e75eae15742
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-28 19:07:20 +0600

    load all user for make an admin and display the users in the ui

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ async function run() {
             return res.send({ success: true, result })
         })
 
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray()
+            res.send(users)
+        })
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
